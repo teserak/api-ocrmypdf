a2a85355a47fd03b7f855c4485b5bc9ee61f5cf4
Konrad Rymczak <konrad.rymczak@gmail.com>
2023-12-11 13:02:13 +0100

    feat: lang-pol

--- api/settings.py
+++ api/settings.py
@@ -8,9 +8,9 @@ class Settings(BaseSettings):
     basedir: Path = Path(os.path.dirname(os.path.abspath(__file__))).resolve()
     workdir: Path = "workdir"
     base_command_ocr: str = "/usr/local/bin/ocrmypdf"
-    api_key_secret: str = "changeme"
+    api_key_secret: str = ""
     base_command_option: str = "--output-type pdf --fast-web-view 0 --optimize 0"
-    max_ocr_process: int = 1
+    max_ocr_process: int = 4
     document_expire_hour: int = 1
     enable_wsl_compat: bool = False
 
